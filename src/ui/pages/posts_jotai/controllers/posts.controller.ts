c0b57d8127621e656ca04877719b4dc181bc6c6f
Arnau Gómez <agomez@mrmilu.com>
2022-12-14 20:54:46 +0100

    feat(posts): create controller

---
+++ src/ui/pages/posts_jotai/controllers/posts.controller.ts
@@ -0,0 +1,12 @@
+import { useLoadPosts, usePostsStore } from "@/src/ui/pages/posts_jotai/state/posts.store";
+import { useEffectStrictMode } from "@front_web_mrmilu/hooks";
+
+export function usePostsController() {
+  const loadPosts = useLoadPosts();
+  useEffectStrictMode(() => {
+    void loadPosts();
+  });
+
+  const postsStore = usePostsStore();
+  return { ...postsStore, loadPosts };
+}
