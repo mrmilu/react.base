2e1a92b0d9e95396d3135e7c74ce1a7a910ee7b5
Guido Ceraso <guidoceraso@gmail.com>
2023-02-10 13:13:30 +0100

    chore: added dockerfile prod image

--- src/common/utils/class-transformer.ts
+++ src/common/utils/class-transformer.ts
@@ -1,6 +1,5 @@
 import { plainToClass } from "class-transformer";
-import { PostDataModel } from "@/src/core/posts/data/models/post_data_model";
 import type { ClassConstructor } from "class-transformer/types/interfaces";
 
 export const fromJson = <T>(model: ClassConstructor<T>, json: Record<string, unknown>) =>
-  plainToClass(PostDataModel, json, { excludeExtraneousValues: true });
+  plainToClass(model, json, { excludeExtraneousValues: true });
