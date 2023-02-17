e4ec669f3a71d7d7f0b18ce0741103af4c2bc99b
Guido Ceraso <guidoceraso@gmail.com>
2023-02-17 14:44:25 +0100

    chore: removed bundle anylzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from "path";
@@ -18,8 +20,6 @@ export default ({ mode }: any) => {
           }
         }
       }),
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
       graphql(),
       react({
         babel: {
