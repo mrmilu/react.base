869d01a4ad72d6ee0e66c003c85398351003a316
Carlos Sala Samper <carlossalasamper@Carloss-MacBook-Pro.local>
2023-02-23 12:49:14 +0100

    Change due to library update

--- src/ui/@types/i18next.d.ts
+++ src/ui/@types/i18next.d.ts
@@ -1,6 +1,6 @@
 import type home from "../i18n/locales/en/home.json";
 
-declare module "react-i18next" {
+declare module "i18next" {
   interface CustomTypeOptions {
     defaultNS: "home";
     resources: {
