367f301492ea74076b94f0d8265beb31a6f3274f
Guido Ceraso <guidoceraso@gmail.com>
2023-03-31 12:34:38 +0200

    chore: added missing constructor type for domain models

--- src/core/posts/domain/models/post.ts
+++ src/core/posts/domain/models/post.ts
@@ -1,9 +1,11 @@
+import type { ConstructorType } from "@/src/common/interfaces/constructor_type";
+
 export class Post {
   id: string;
   title: string;
   body: string;
 
-  constructor(params: { id: string; title: string; body: string }) {
+  constructor(params: ConstructorType<Post>) {
     this.id = params.id;
     this.title = params.title;
     this.body = params.body;
