7c366f0515650e447c0557f963eb1787f1305a22
Guido Ceraso <guidoceraso@gmail.com>
2023-06-05 07:35:20 +0200

    chore: added possibilty to skip middleware from validation hook

--- src/ui/router/route_middleware.tsx
+++ src/ui/router/route_middleware.tsx
@@ -2,13 +2,13 @@ import { Await, Navigate, useLocation } from "react-router-dom";
 import type { TrackedPromise } from "@remix-run/router";
 import { defer } from "react-router-dom";
 import { SuspenseMainLoader } from "@/src/ui/components/suspense_main_loader/suspense_main_loader";
-import * as React from "react";
 
 export interface ValidationHookOutput {
   errorRedirectUrl?: string;
   successRedirectUrl?: string;
-  promise: Promise<any>;
+  promise: Promise<unknown>;
   replace?: boolean;
+  skipMiddleware?: boolean;
 }
 
 export type MiddlewareHook = () => ValidationHookOutput;
@@ -22,24 +22,26 @@ interface Props {
 export const trackPromise = <T,>(promise: Promise<T>): TrackedPromise => defer({ promise }).data.promise as TrackedPromise;
 
 export const RouteMiddleware = ({ children, validationHook }: Props) => {
+  const { errorRedirectUrl, replace = true, successRedirectUrl, promise, skipMiddleware } = validationHook();
   const location = useLocation();
-  if (location.state?.skipMiddleware) {
+
+  if (location.state?.skipMiddleware || skipMiddleware) {
     window.history.replaceState({}, document.title);
     return children;
   }
-  const { errorRedirectUrl, replace = true, successRedirectUrl, promise } = validationHook();
 
-  const NavigateRedirect = (props: { redirectUrl: string }) => (
-    <Navigate
-      to={props.redirectUrl}
-      state={{
-        from: location,
-        skipMiddleware: true
-      }}
-      replace={replace}
-    />
-  );
-
+  const NavigateRedirect = (props: { redirectUrl: string }) => {
+    return (
+      <Navigate
+        to={props.redirectUrl}
+        state={{
+          from: location,
+          skipMiddleware: true
+        }}
+        replace={replace}
+      />
+    );
+  };
   return (
     <SuspenseMainLoader>
       <Await resolve={trackPromise(promise)} errorElement={errorRedirectUrl ? <NavigateRedirect redirectUrl={errorRedirectUrl} /> : children}>
